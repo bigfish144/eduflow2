4ced36dacba608e6d09f26d58fea06b55bfe6bb8
wudanni <425938500@qq.com>
2025-01-26 22:51:52 +0800

    Your commit message

--- utils.py
+++ utils.py
@@ -9,4 +9,4 @@ def image_to_base64(filename):
     with open(image_path, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
     return encoded_string
-
+#test
