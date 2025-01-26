0414bbe5094a9e67cb86290b5fbc2a242734e551
wudanni <425938500@qq.com>
2025-01-26 22:43:17 +0800

    test

--- utils.py
+++ utils.py
@@ -3,11 +3,10 @@ import base64
 def load_json_template(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return json.load(file)
- 
+# 打开图片文件并读取其二进制内容，进行Base64编码，然后解码为UTF-8字符串
 def image_to_base64(filename):
     image_path = f"./output/{filename}"    
     with open(image_path, "rb") as image_file:
-        # 打开图片文件并读取其二进制内容，进行Base64编码，然后解码为UTF-8字符串
         encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
     return encoded_string
 
